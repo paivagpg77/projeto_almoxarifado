46b4fc449f77033924e3a5d98fe6a3c45fc3d26f
joaopedrooo <joaopedrosouzarodriguesj@gmail.com>
2026-03-07 20:39:14 -0300

    Resolvendo conflitos no ambiente virtual

--- models/produtos.py
+++ models/produtos.py
@@ -1,11 +1,11 @@
 class Produto: 
-    def __init__(self, id, nome, descricao, unidade, estoque_minimo, id_categoria, quantidade_atual, ativo):
+    def __init__(self, id, nome, descricao, unidade, estoque_minimo, id_categoria, quantidade_atual = 0, ativo=True):
         self.id = id
         self.nome = nome
         self.descricao = descricao
         self.unidade = unidade
         self.estoque_minimo = estoque_minimo
         self.id_categoria = id_categoria
-        self.quantidade_atual = 0
-        self.ativo = True
+        self.ativo = ativo
+        self.quantidade_atual = quantidade_atual
         
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\meus-projetos\projeto_do_paiva_errado\venv
+set VIRTUAL_ENV=C:\meus-projetos\projeto_almoxarifado\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
