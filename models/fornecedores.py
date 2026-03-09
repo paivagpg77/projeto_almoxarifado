9f7a3126551b740a081cb9f803809b709676bd25
vitor <vitor242010@gmail.com>
2026-03-09 07:44:54 -0300

    validacao de fornecedores

--- models/fornecedores.py
+++ models/fornecedores.py
@@ -3,15 +3,21 @@ class Fornecedor:
     def __init__(self, nome: str, cnpj: str, telefone: str, email: str, ativo: bool = True):
         if not nome:
             raise ValueError ("Nome inválido")
-        self.nome = nome
+        self.nome = nome.strip().title()
+        
         if not verify_email.validar_email(email):
             raise ValueError ("Email inválido")
-        self.email = email
+        self.email = email.lower()
+
         if not validador_cnpj.validar_cnpj(cnpj):
             raise ValueError ("Cnpj Inválido")
-        self.cnpj = cnpj
-        
-        
-        
+        self.cnpj = cnpj.replace(".", "").replace("/" , "").replace("-" ,"")
+
+        if not telefone or not telefone.strip():
+            raise ValueError ("Telefone Inválido")
+        self.telefone = telefone.strip()
+
+        self.ativo = ativo 
+
         
         
